8c509d30fd2ab1c06652df7227acb30b79dadcf9
Boyd smit <boydsmit@live.nl>
2018-09-11 13:51:01 +0200

    Project Start

---
+++ Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+
+	public class InputManager : MonoBehaviour
+	{
+
+		private PlayerMove _playerMove;
+
+		// Update is called once per frame
+
+		private void Start()
+		{
+			_playerMove = GetComponent<PlayerMove>();
+		}
+
+		void Update()
+		{
+			if (Input.GetKey(KeyCode.LeftShift))
+			{
+				_playerMove.Slowmode();
+
+			}
+			else
+			{
+				_playerMove.NormalMode();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+
+	public class PlayerMove : MonoBehaviour
+	{
+
+		private Rigidbody2D _rb;
+
+		[SerializeField] private float _initialMoveSpd;
+
+		private float _moveSpeed;
+
+		void Start()
+		{
+			_rb = GetComponent<Rigidbody2D>();
+			_moveSpeed = _initialMoveSpd;
+
+		}
+
+		void Move(float x, float y)
+		{
+			_rb.velocity = new Vector2(x, y).normalized * _moveSpeed * Time.deltaTime;
+		}
+
+		void Update()
+		{
+
+			float x = Input.GetAxisRaw("Horizontal");
+			float y = Input.GetAxisRaw("Vertical");
+			Move(x, y);
+		}
+
+		public void Slowmode()
+		{
+			_moveSpeed = _initialMoveSpd / 2;
+		}
+
+		public void NormalMode()
+		{
+			_moveSpeed = _initialMoveSpd;
+		}
+	}
+}
