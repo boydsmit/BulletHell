5bd03d6ede1bf9cd1fde84a1a8c8772aa218e1d4
Boyd smit <boydsmit@live.nl>
2018-09-12 14:58:37 +0200

    +Created ScriptableObject script "Enemies" +Created CheckEnemy script +Created a Scriptable Enemy test

---
+++ Assets/Scripts/Enemy/CheckEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Enemy;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Enemy
+{
+
+	public class CheckEnemy : MonoBehaviour
+	{
+
+		public Enemies EnemyType;
+
+		private int _health;
+		private int _weapons;
+
+		public Image Sprite;
+		
+		void Start()
+		{
+			_health = EnemyType.Health;
+			_weapons = EnemyType.Weapons;
+
+			
+			Sprite.sprite = EnemyType.SpriteImage;
+
+			print(_health + " " + _weapons);
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/Enemy/Enemies.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+namespace Enemy
+{
+	[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemies" )]	
+	public class Enemies : ScriptableObject
+	{
+		public int Health;
+		public int Weapons;
+
+		public Sprite SpriteImage;
+	}
+}
